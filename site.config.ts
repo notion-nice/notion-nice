54acc92767170e44e13586ccb35b97642e7f4610
SengMitnick <sengmitnick@163.com>
2024-03-02 18:28:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -42,11 +42,9 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/privacy': '94d0ba8c65ea40e2b6455b8d1037752e'
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
