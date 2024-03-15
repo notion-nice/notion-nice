b17b67d01aeac7c88f5cfce0afb578d4cc75c5f9
SengMitnick <sengmitnick@163.com>
2024-03-15 21:03:49 +0800

    update /api/stripe/:path* Access-Control-Allow-Origin

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ module.exports = withBundleAnalyzer({
         // matching all API routes
         source: '/api/stripe/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Origin', value: 'chrome-extension://*' }, // replace this your actual origin
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT'
