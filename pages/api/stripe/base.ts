a0b2f07a0be5a826e6f27854f3de9976bd8a4127
SengMitnick <sengmitnick@163.com>
2024-03-24 00:55:14 +0800

    Create base.ts

---
+++ pages/api/stripe/base.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const notionClientVersion = '23.13.0.146'
+const comboList = {
+  free: [
+    '增添 Notion 文章大纲目录',
+    '文章一键排版到公号等平台',
+    '不支持上传图片'
+  ],
+  plus: ['支持无限制图片上传', '访问额外的实验性功能（开发中）']
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  if (req.method !== 'POST') {
+    return res.status(405).send({ error: 'method not allowed' })
+  }
+  return res.send({ ok: true, comboList, notionClientVersion })
+}
