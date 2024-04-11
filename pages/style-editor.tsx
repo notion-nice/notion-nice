71e581ded2721b3b0c7c582687749349db495149
SengMitnick <sengmitnick@163.com>
2024-04-11 11:14:27 +0800

    update StyleEditor

--- pages/style-editor.tsx
+++ pages/style-editor.tsx
@@ -1,6 +1,8 @@
 import { css } from '@codemirror/lang-css'
-import { EditorState } from '@codemirror/state'
+import { Compartment, EditorState } from '@codemirror/state'
 import { EditorView, lineNumbers } from '@codemirror/view'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { throttle } from 'lodash-es'
 import { minimalSetup } from 'codemirror'
 import React, { useEffect, useRef } from 'react'
 
@@ -9,41 +11,30 @@ const StyleEditor = () => {
   const editor = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    function onmessage(event: MessageEvent) {
-      if (event.origin !== 'https://www.notion.so') return
-      if (event.data?.s !== 'notion-nice') return
-      const cm = editorView.current
-      if (cm === null) return
-      const { type, value } = event.data
-      switch (type) {
-        case 'setDoc':
-          cm.dispatch({
-            changes: { from: 0, to: cm.state.doc.length, insert: value }
-          })
-          break
-        case 'setEditable':
-          //   cm.dispatch({
-          //     effects: EditorView.editable.set(value)
-          //   })
-          break
-
-        default:
-          break
-      }
-    }
-    window.addEventListener('message', onmessage)
-    return () => {
-      window.removeEventListener('message', onmessage)
-    }
-  }, [])
-
-  useEffect(() => {
+    const editable = true
+    const enableAction = new Compartment()
+    const editorTheme = new Compartment()
+    const oneLight = EditorView.theme({})
     const theme = EditorView.theme({
       '&': {
         height: '100vh',
         minHeight: '200px'
       },
       '.cm-scroller': { overflow: 'auto' }
+    })
+
+    const onUpdate = throttle((doc: string) => {
+      window.parent.postMessage(
+        { s: 'notion-nice', type: 'save', value: doc },
+        '*'
+      )
+    }, 800)
+
+    const updateListener = EditorView.updateListener.of((vu) => {
+      if (vu.docChanged) {
+        const doc = vu.state.doc
+        onUpdate(doc.toString())
+      }
     })
 
     const startState = EditorState.create({
@@ -53,38 +44,62 @@ const StyleEditor = () => {
         theme,
         minimalSetup,
         lineNumbers(),
-        EditorView.lineWrapping
+        updateListener,
+        EditorView.lineWrapping,
+        editorTheme.of(oneLight),
+        enableAction.of([
+          EditorView.editable.of(editable),
+          EditorState.readOnly.of(!editable)
+        ])
       ]
     })
-    const view = new EditorView({
+    const cm = new EditorView({
       state: startState,
       parent: editor.current
     })
+    editorView.current = cm
 
-    editorView.current = view
+    function onmessage(event: MessageEvent) {
+      if (event.origin !== 'https://www.notion.so') return
+      if (event.data?.s !== 'notion-nice') return
+      const { type, value } = event.data
+      switch (type) {
+        case 'setDoc':
+          cm.dispatch({
+            changes: { from: 0, to: cm.state.doc.length, insert: value }
+          })
+          break
+        case 'setEditable':
+          cm.dispatch({
+            effects: [
+              enableAction.reconfigure([
+                EditorView.editable.of(!!value),
+                EditorState.readOnly.of(!value)
+              ])
+            ]
+          })
+          break
+        case 'selectedTheme':
+          cm.dispatch({
+            effects: this.editorTheme.reconfigure(
+              value === 'dark' ? oneDark : oneLight
+            )
+          })
+          break
+
+        default:
+          break
+      }
+    }
+    window.addEventListener('message', onmessage)
 
     return () => {
-      view.destroy()
+      cm.destroy()
+      window.removeEventListener('message', onmessage)
     }
   }, [])
 
-  return (
-    <div>
-      <div ref={editor} className='relative flex-none w-full h-full'></div>
-      <button
-        onClick={() => {
-          const cm = editorView.current
-          if (cm === null) return
-          window.parent.postMessage(
-            { s: 'notion-nice', type: 'save', value: cm.state.doc.toString() },
-            '*'
-          )
-        }}
-      >
-        Save
-      </button>
-    </div>
-  )
+  return <div ref={editor} className='relative flex-none w-full h-full'></div>
 }
 
 export default StyleEditor
